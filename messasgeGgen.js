417d690948eaca5ceb464a2c25f8c37f1710d036
Dave <david.gomez.m@hotmail.com>
2024-10-07 14:57:04 -0600

    Se corrije la extension del archivo del script principal

---
+++ messasgeGgen.js
@@ -0,0 +1,13 @@
+function generarNumeroRandom(num){
+    //Obitne un numeor entre rango de 0 y -1
+    return Math.floor(Math.random() * num);
+}
+
+
+//Se declaran las listas de actividades.
+const listaDeActividades = {
+  montana: ['Senderismo','Alpinismo','Tirolesa','Decesno a rapel'],
+  lago: ['Cayack','Natacion','Buceo'],
+  trabajo: ['Limpieza','Cocina','Ordenar Almacen','Guaradia']
+}
+
